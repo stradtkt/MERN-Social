83ddbf9110af937d668f717386df604f42713f24
Kevin <stradtkt22@gmail.com>
2019-05-25 03:27:11 -0400

    Added navbar and landing pages

---
+++ client/social/src/components/layout/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+        return (
+            <nav className="navbar bg-dark">
+                <h1><a href="index.html"><i className="fas fa-code"></i>DevConnector</a></h1>
+            </nav>
+        )
+}
+export default Navbar;
