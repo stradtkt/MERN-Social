ac0ab82101ac471ae35a04c7627518ed325e797d
Kevin <stradtkt22@gmail.com>
2019-05-25 01:34:51 -0400

    Added authentication to user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('./../../middleware/auth');
 const User = require('./../../models/User');
+const bcrypt = require('bcryptjs');
 
 router.get('/', auth, async (req, res) => {
     try {
@@ -12,5 +13,37 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+try {
+    let user = await User.findOne({email});
+    if(!user) {
+        return res
+            .status(400)
+            .json({errors: [{msg: 'Invalid Credentials'}]});
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) {
+        return res
+            .status(400)
+            .json({errors: [{msg: 'Invalid Credentials'}]});
+    }
+    const payload = {
+        user: {
+            id: user.id
+        }
+    }
+    jwt.sign(
+        payload, 
+        config.get('jwtSecret'), 
+        {
+            expiresIn: 3600000
+        }, 
+        (err, token) => {
+            if(err) throw err;
+            res.json({token});
+        })
+} catch(err) {
+    console.log(err.message);
+    res.status(400).send('Server Error');
+}
 
 module.exports = router;
