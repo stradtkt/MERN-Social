a5bdec7ae32791183460d4c74f92e805bb981da8
Kevin <stradtkt22@gmail.com>
2019-05-25 02:00:22 -0400

    Added get route to the profile routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send('Posts route');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
+        if(!profile) {
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        }
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
