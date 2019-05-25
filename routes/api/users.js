1ae653f4b9265a0bc521393598fc5a6f7fb09b8b
Kevin <stradtkt22@gmail.com>
2019-05-25 00:09:19 -0400

    Added user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const {check, validationResult} = require('express-validator/check');
+const User = require('../../models/User');
 router.post('/', 
     [
         check('name', 'Name is required')
@@ -14,7 +17,30 @@ router.post('/',
         if(!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()});
         }
-        res.send('User route');
+        const {name, email, password} = req.body;
+        try {
+            let user = await User.findOne({email});
+            if(user) {
+                res.status(400).json({errors: [{msg: 'User already exists'}]});
+            }
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
 });
 
 module.exports = router;
