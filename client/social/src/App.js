314eb15128e59e4bee145324488442fa433f527e
Kevin <stradtkt22@gmail.com>
2019-05-29 01:51:15 -0400

    Added forms

--- client/social/src/App.js
+++ client/social/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -33,6 +34,7 @@ const App = () => {
                             <Route exact path='/login' component={Login}/>
                             <Route exact path='/register' component={Register}/>
                             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+                            <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                         </Switch>
                     </section>
                     <Landing/>
