d85b5252608fcf802803233648cc962130a6d843
Kevin <stradtkt22@gmail.com>
2019-06-01 01:26:51 -0400

    Added more to the profile

--- client/social/src/App.js
+++ client/social/src/App.js
@@ -9,6 +9,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Alert from './components/layout/Alert';
+import Profile from './components/profile/Profile';
 import Profiles from './components/profiles/Profiles';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -38,6 +39,7 @@ const App = () => {
                             <Route exact path='/login' component={Login}/>
                             <Route exact path='/register' component={Register}/>
                             <Route exact path='/profiles' component={Profiles}/>
+                            <Route exact path='/profile' component={Profile}/>
                             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
                             <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
--- client/social/src/components/dashboard/Dashboard.js
+++ client/social/src/components/dashboard/Dashboard.js
@@ -11,7 +11,7 @@ import Education from './Education';
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile, loading}, deleteAccount}) => {
     useEffect(() => {
         getCurrentProfile();
-    }, []);
+    }, [getCurrentProfile]);
     return loading && profile === null ? (<Spinner/>) : 
         (<Fragment>
             <h1 className="large text-primary">Dashboard</h1>
