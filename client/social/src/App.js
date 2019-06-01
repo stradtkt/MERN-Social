65c6b4816215c9176ff25348e6d5cf32e4f04ddb
Kevin <stradtkt22@gmail.com>
2019-06-01 00:14:24 -0400

    Getting the profile together

--- client/social/src/App.js
+++ client/social/src/App.js
@@ -9,6 +9,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Alert from './components/layout/Alert';
+import Profiles from './components/profiles/Profiles';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import setAuthToken from './utils/setAuthToken';
@@ -36,6 +37,7 @@ const App = () => {
                         <Switch>
                             <Route exact path='/login' component={Login}/>
                             <Route exact path='/register' component={Register}/>
+                            <Route exact path='/profiles' component={Profiles}/>
                             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
                             <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                             <PrivateRoute exact path='/edit-profile' component={EditProfile}/>
