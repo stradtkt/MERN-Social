fbf9422ea7b66465d97e823473e23ea1fd59cf5e
Kevin <stradtkt22@gmail.com>
2019-06-03 08:38:34 -0400

    Added to post a post route

--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProfileSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'users'
     },
     company: {
         type: String
